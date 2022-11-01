52e070d8e61a435eff60a7612309e287411b70fe
Goltzishpt <gilzagoltz@gmail.com>
2022-11-01 17:31:46 +0200

    скобочка

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 PROJECT_ROOT = path.join(path.abspath(__file__))
 STATIC_ROOT = path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [path.join(BASE_DIR, 'static'),
                     path.join(PROJECT_ROOT, 'static'), ]
 
